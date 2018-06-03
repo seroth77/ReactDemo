3a4337443289efc419a84d43f2ebf957b19bd374
sero <seroth77@gmail.com>
2018-06-03 14:36:54 -0400

    init push

---
+++ movies_search/src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+//import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        
+        <div className="container-fluid">
+          <div className="Main-Header">
+            <div className="row">
+              <div className="col-sm-12">
+                <span className="fa fa-film"></span>
+                <h3>MoviesDB Search</h3>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-sm-12">
+              cell 2
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
